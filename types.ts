af6a10fd49c43182508210ada50267e2ff4509fc
cool8522 <prathamswarnkar768@gmail.com>
2024-11-15 20:02:00 +0530

    beforevideo

--- types.ts
+++ types.ts
@@ -2,7 +2,7 @@ import { Server as NetServer, Socket } from "net";
 import { NextApiRequest } from "next";
 import {Server as SocketIOServer} from "socket.io";
 import { Server, Member, Profile } from "@prisma/client"
-
+import { Message } from "@prisma/client"
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })[];
 };
